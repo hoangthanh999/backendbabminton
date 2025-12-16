08fc1567451aa7bd0e211d8f30b7e6b552544da5
Hoang Thanh Hong <hoanghong76543@gmail.com>
2025-12-16 10:26:38 +0700

    first commit

---
+++ src/main/java/com/badminton/entity/base/AuditableEntity.java
@@ -0,0 +1,22 @@
+package com.badminton.entity.base;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class AuditableEntity extends BaseEntity {
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    private Long createdBy;
+
+    @LastModifiedBy
+    @Column(name = "modified_by")
+    private Long modifiedBy;
+}
